3ef5d0198b7681a6a70160f4be7135a10e1d23a4
Divya Panackal <divyap@qburst.com>
2021-04-30 13:50:01 +0530

    code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'publish'),
     filename: 'bundle.js',
   },
   module: {
@@ -38,7 +38,7 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, 'publish'),
     watchContentBase: true,
   },
 };
